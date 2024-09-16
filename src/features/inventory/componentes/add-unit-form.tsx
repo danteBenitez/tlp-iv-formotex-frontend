8f49adcc94b593e2d547a0ff512385cdee693307
danteBenitez <benitezdante123@gmail.com>
2024-09-16 09:33:39 -0300

    fix: Mostrar mensaje error al intentar crear unidad sin organizaciones disponibles

--- src/features/inventory/componentes/add-unit-form.tsx
+++ src/features/inventory/componentes/add-unit-form.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { useQuery } from "@tanstack/react-query";
 import { PlusIcon } from "lucide-react";
 import { UseFieldArrayReturn } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod";
 import { equipmentWithUnitsSchema } from "../schema/equipment-with-units";
 import { getOrganizations } from "../services/inventory";
@@ -21,12 +22,18 @@ export function AddUnitForm(props: {
         className=""
         variant="secondary"
         onClick={() => {
+          if (organizations && organizations.length === 0) {
+            toast.error(
+              "No se pudieron encontrar organizaciones. Por favor, cree una antes de insertar una unidad"
+            );
+            return;
+          }
           props.units.append({
             acquiredAt: new Date(),
             location: "",
             state: "ok",
             serialNumber: 0,
-            organizationId: parseInt(organizations?.[0].organizationId ?? "1"),
+            organizationId: parseInt(organizations?.[0]?.organizationId ?? ""),
           });
         }}
       >
