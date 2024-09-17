a624ca98979f46833b95e9f36fc6039c7e975a80
danteBenitez <benitezdante123@gmail.com>
2024-09-16 22:49:31 -0300

    fix: Eliminar la posibilidad de registrarse normalmente

--- src/pages/auth/register.tsx
+++
@@ -1,9 +0,0 @@
-import RegisterForm from "@/features/auth/components/register-form";
-
-export default function RegisterPage() {
-  return (
-    <main className="flex items-center justify-center h-full w-full min-h-screen">
-      <RegisterForm />
-    </main>
-  );
-}
